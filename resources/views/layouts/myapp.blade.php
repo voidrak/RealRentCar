dd6714c2fed743ab0896b00499b6adcfc0819190
RohaTech <raklovetocode@gmail.com>
2025-02-22 09:44:27 +0300

    fix: update logo images and remove unused assets

--- resources/views/layouts/myapp.blade.php
+++ resources/views/layouts/myapp.blade.php
@@ -30,7 +30,7 @@
                 <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl drop-shadow-2xl">
                     {{-- LOGO --}}
                     <a href="{{ route('home') }}" class="flex items-center">
-                        <img loading="lazy" src="/images/carLogo.png" class="mr-3 h-12" alt="Flowbite Logo" />
+                        <img loading="lazy" src="/images/SmallSquareLogo.jpg" class="mr-3 h-12" alt="Flowbite Logo" />
                     </a>
 
                     {{-- login & Register buttons --}}
@@ -326,7 +326,8 @@
                 <div class="md:flex md:justify-between">
                     <div class="mb-12 md:mb-0 flex justify-center ">
                         <a href="" class="flex items-center">
-                            <img loading="lazy" src="/images/carLogo.png" class="mr-3 h-24" alt="Logo" />
+                            <img loading="lazy" src="/images/SmallSquareLogo.jpg" class="mr-3 h-24"
+                                alt="Logo" />
                         </a>
                     </div>
 
@@ -373,18 +374,11 @@
                 <hr class="my-6  sm:mx-auto border-gray-700 lg:my-8" />
 
                 <div class="sm:flex sm:items-center sm:justify-between md:ms-0 pb-4  ms-32">
-                    <span class="text-sm sm:text-center text-gray-400 md:ms-0 -ms-8">© 2022 <a
-                            href="https://www.linkedin.com/in/mohamed-galdi/" class="hover:underline"> </a> All Rights
+                    <span class="text-sm sm:text-center text-gray-400 md:ms-0 -ms-8">© 2022 <a href="#"
+                            class="hover:underline"> </a> All Rights
                         Reserved.
                     </span>
                     <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-                        <a href="#" class="text-gray-500 hover:text-white">
-                            <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                                <path fill-rule="evenodd"
-                                    d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                        </a>
                         <a href="#" class="text-gray-500 hover:text-white">
                             <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path
